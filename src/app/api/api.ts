c3e3334f6ccd50c00b15564a3d3c64163f8e075d
Jonatan I <aguilarupch@hotmail.com>
2019-12-10 03:16:20 -0600

    Added Travis CI automation!

---
+++ src/app/api/api.ts
@@ -0,0 +1,9 @@
+export class API {
+    
+    public host = "http://127.0.0.1:8080"
+    public employees = this.host+"/api/users";
+    public employee = this.host+"/api/user";
+    constructor(){}
+ 
+ 
+ }
